6d60636c88fb9f6a8f4adeb090c93dc842283689
ARYAN YADAV <96650638+aryan9700@users.noreply.github.com>
2023-02-11 13:00:06 +0530

    first commit

---
+++ Main.R
@@ -0,0 +1,13 @@
+print("hello") #for print
+Name <- "Aryan" #leftward operator
+print(Name)
+Names = 23  #assignment operator
+print(Name)
+89 ->Nam #rightward operator
+print(Name)
+class(Name) #show datatype of variable
+class(Names)
+class(Nam)
+ls() #to list all variable
+rm(Name) #to remove variable
+ls()
